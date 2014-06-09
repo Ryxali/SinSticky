7f3d7d021044c0223b75d7b561e4ed6fb9fa2c2b
Sebastian <a13sebja@student.his.se>
2014-06-09 22:18:15 +0200

    Line

--- Assets/LineController.cs
+++ Assets/LineController.cs
@@ -2,14 +2,34 @@
 using System.Collections;
 
 public class LineController : MonoBehaviour {
+	public GameObject player;
+	public GameObject target;
 
-	// Use this for initialization
-	void Start () {
-	
+	/*public Color c1 = Color.yellow;
+	public Color c2 = Color.red;
+	public int lengthOfLineRenderer = 20;
+	void Start() {
+		LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		lineRenderer.SetColors(c1, c2);
+		lineRenderer.SetWidth(0.2F, 0.2F);
+		lineRenderer.SetVertexCount(lengthOfLineRenderer);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update() {
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		int i = 0;
+		while (i < lengthOfLineRenderer) {
+			Vector3 pos = new Vector3(i * 0.5F, Mathf.Sin(i + Time.time), 0);
+			lineRenderer.SetPosition(i, pos);
+			i++;
+		}
+	}*/
+
+
+	void FixedUpdate()
+	{
+		LineRenderer lineRenderer = GetComponent<LineRenderer> ();
+		transform.position = player.rigidbody.position;
+		lineRenderer.SetPosition (0, target.transform.position);
 	}
 }
