40208c3b795c12de33b88d62c58efb1d9c8835d2
Sebastian <a13sebja@student.his.se>
2014-06-10 11:54:21 +0200

    Text rotate

---
+++ Assets/Scripts/HookChecker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HookChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TongueController c = GameObject.Find ("TongueEnd").GetComponent<TongueController>();
+		print (c.getStage ().ToString());
+	}
+}
