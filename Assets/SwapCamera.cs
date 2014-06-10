e0e789da6d72e242a02aa8c5219f6007c2abaa71
Ryxali <ryxali@gmail.com>
2014-06-10 11:53:27 +0200

    Camera sequences added

--- Assets/SwapCamera.cs
+++ Assets/SwapCamera.cs
@@ -2,44 +2,44 @@
 using System.Collections;
 
 public class SwapCamera : MonoBehaviour {
-	public GameObject tongue;
 	public Camera firstPersonCamera;
 	public Camera thirdPersonCamera;
 	public Camera hookshotCamera;
 	private bool inHookshotMode = false;
+	private TongueController controller;
 	// Use this for initialization
 	void Start () {
 		firstPersonCamera.enabled = true;
 		renderer.enabled = false;
 		thirdPersonCamera.enabled = false;
+		controller = GameObject.Find ("TongueEnd").GetComponent<TongueController> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
+		TongueController.Stage stage = controller.getStage ();
 		if (Input.GetButtonDown ("Fire1")) {
 			inHookshotMode = true;
 		}
-		if (tongue.GetComponent<TongueController> ().getStop ()) {
-			firstPersonCamera.enabled = false;
-			thirdPersonCamera.enabled = false;
-			hookshotCamera.enabled = true;
-			/*bool tongueStop = tongue.GetComponent<TongueController> ().getStop ();
-			if(tongueStop) {
-
-			}
-			else
-			{
-				renderer.enabled = true;
-				firstPersonCamera.enabled = false;
-				hookshotCamera.enabled = false;
-				thirdPersonCamera.enabled = true;
-			}*/
-
-		} else {
+		switch (stage) {
+		case TongueController.Stage.standby:
 			renderer.enabled = false;
 			thirdPersonCamera.enabled = false;
 			hookshotCamera.enabled = false;
 			firstPersonCamera.enabled = true;
+			break;
+		case TongueController.Stage.shooting:
+			firstPersonCamera.enabled = false;
+			thirdPersonCamera.enabled = false;
+			renderer.enabled = false;
+			hookshotCamera.enabled = true;
+			break;
+		case TongueController.Stage.reeling:
+			firstPersonCamera.enabled = false;
+			hookshotCamera.enabled = false;
+			thirdPersonCamera.enabled = true;
+			renderer.enabled = true;
+			break;
 		}
 	}
 }
