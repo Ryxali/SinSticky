9ef19b4df86a6b665f904fba2fef1767a4dc80ac
Koutyboy <victor.karlsson.github@gmail.com>
2014-06-10 20:46:45 +0200

    Added working stuff

---
+++ Assets/WinController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinController : MonoBehaviour {
+	private int 	activeCoffee;
+	public 	string 	nextLevel;
+
+	public void AddCoffee() {
+		activeCoffee++;
+	}
+
+	public void SubCoffee() {
+		activeCoffee--;
+		if (activeCoffee <= 0) {
+			Win ();
+		}
+	}
+
+	void Win() {
+		Application.LoadLevel (nextLevel);
+	}
+}
