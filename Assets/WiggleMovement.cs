cb95012e8723e5ffccdbab7c748ace4765d5a010
Ryxali <ryxali@gmail.com>
2014-06-10 18:53:16 +0200

    wiggled that waggle

---
+++ Assets/WiggleMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WiggleMovement : MonoBehaviour {
+	public float force = 100;
+	public float upForce = 100;
+	public float torqueForce = 60;
+	public Camera cam;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetButtonDown ("Fire2")) {
+			rigidbody.AddForce (cam.transform.forward * force + Vector3.up*upForce);
+			rigidbody.AddTorque (cam.transform.right*torqueForce);
+		}
+	}
+}
