1e05ae46bf01aa86d4cf4091d05de3197bf31643
Sebastian <a13sebja@student.his.se>
2014-06-11 18:04:36 +0200

    Police

---
+++ Assets/PoliceController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoliceController : MonoBehaviour {
+	public Rigidbody[] rigidBodies;
+	private bool ragdolled;
+	// Use this for initialization
+	void Start () {
+		ragdolled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player" && ragdolled == false)
+		{
+			Debug.Log("Player-Police collision");
+			Animator animator = GetComponent<Animator> ();
+			animator.enabled = false;
+			foreach (Rigidbody body in rigidBodies)
+			{
+				body.isKinematic = false;
+				Debug.Log("Blubb");
+			}
+			ragdolled = true;
+		}
+	}
+}
